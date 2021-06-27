6f232982a2504b499507d27288b8c864b7901c08
Shalini Basak <basakshalini123@gmail.com>
2021-06-27 20:05:42 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 var http = require("http");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = 5000 || process.env.PORT;
 var server = http.createServer(app);
 var io = require("socket.io")(server);
 
