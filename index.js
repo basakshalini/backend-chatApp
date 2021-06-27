907575cdb9456182f54dc8d59d67b71711bbf6e1
Shalini Basak <basakshalini123@gmail.com>
2021-06-27 19:07:26 +0530

    heroku deployment1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+var http = require("http");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+app.use(express.json());
+var clients = {};
+//app.use(cors());
+
+io.on('connection',(socket) => {
+    console.log('connected');
+    console.log(socket.id , "has joined");
+    socket.on("signin" , (id) => {
+        console.log(id);
+        clients[id] = socket;
+        console.log(clients);
+    });
+    socket.on("message" , (msg) => {
+        console.log(msg);
+        let targetId = msg.targetId;
+    if (clients[targetId]) clients[targetId].emit("message", msg); 
+    });
+
+});
+app.route("/check").get((req,res) => {
+    return res.json("Your app is working fine");
+})
+
+server.listen(port, "0.0.0.0",()=> {
+    console.log('Server started');
+});
